7b42bd1f6cfa9dddc93e332b1ade85159f32ffa2
loique <>
2024-05-07 06:03:50 -0700

    main.ts file modification

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+const corsConfig = {
+  origin: 'http://localhost:4200',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    credentials: true,
-  });
+  app.enableCors(corsConfig);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
